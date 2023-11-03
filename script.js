5e03824ffea38e9b6e70a06959e9174029b2428f
TarasLys <opros0609@gmail.com>
2023-11-03 20:42:26 +0200

    finish

--- script.js
+++ script.js
@@ -51,5 +51,31 @@ window.onload = function () {
             coordXprocent = coordX / parallaxWidth * 100;
             coordYprocent = coordY / parallaxHeight * 100;
         });
+
+
+
+        let thresholdSets = [];
+        for (let i = 0; i <= 1.0; i += 0.005) {
+            thresholdSets.push(i);
+        }
+        const callback = function (entries, observer) {
+            const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
+            setParallaxItemsStyle(scrollTopProcent);
+        };
+        const observer = new IntersectionObserver(callback, {
+            threshold: thresholdSets
+        });
+    
+        observer.observe(document.querySelector(".content"));
+
+        function setParallaxItemsStyle(scrollTopProcent) {
+            content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%);`;
+            mountains.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%);`;
+            human.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
+        }
+    
+    
     }
+
+
 }
